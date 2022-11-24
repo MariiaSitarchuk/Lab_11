8e83cb7672692773330d13a369f87c122064a064
MariiaSitarchuk <92577191+MariiaSitarchuk@users.noreply.github.com>
2022-11-24 11:33:21 +0200

    Update GiftMail.java

--- src/main/java/task_2/GiftMail.java
+++ src/main/java/task_2/GiftMail.java
@@ -1,1 +1,10 @@
+package task_2;
 
+public class GiftMail implements MailCode {
+  private static final String TEMPLATE = "BLACK FRIDAY! %NAME come and get your gift right now!";
+  
+  @Override
+  public String generate(Client client) {
+    return TEMPLATE.replaceAll("%NAME", client.getName());
+  }
+}
