f872633d2ab0fa863f7037181c9556cc05e57206
MariiaSitarchuk <92577191+MariiaSitarchuk@users.noreply.github.com>
2022-11-24 11:22:58 +0200

    Update MailInfo.java

--- src/main/java/task_2/MailInfo.java
+++ src/main/java/task_2/MailInfo.java
@@ -1,5 +1,13 @@
 package task_2;
 
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
 public class MailInfo {
+  private Client client;
+  private MailCode mailCode;
   
+  public String generate() {
+    return mailCode.generate(client);
+  }
 }
